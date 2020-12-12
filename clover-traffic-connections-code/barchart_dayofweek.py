4e2c7c8eb3dd7a916d5112188a680fad81f5774c
Rachel Tomas <rtomas2@uncc.edu>
2020-12-11 21:36:22 -0500

    adding barchart with day of week for crashes with injury

---
+++ clover-traffic-connections-code/barchart_dayofweek.py
@@ -0,0 +1,23 @@
+import dash
+import pandas as pd
+import plotly.offline as pyo
+import plotly.graph_objs as go
+
+# Load CSV file from Datasets folder
+df = pd.read_csv('../datasets/monroe-county-crash-data2003-to-2015.csv', encoding="ISO-8859–1")
+
+# Filtering US Cases
+filtered_df = df[df['Injury Type'] != 'No injury/unknown']
+
+# Creating sum of number of cases group by State Column
+new_df = filtered_df.groupby(['Day'])['Injury Type'].count().reset_index()
+
+# Preparing data
+data = [go.Bar(x=new_df['Day'], y=new_df['Injury Type'])]
+
+# Preparing layout
+layout = go.Layout(title='Number of Crashes with Injury in Bloomington by Day of Week', xaxis_title="Day of the Week", yaxis_title="Number of Crashes with Injury")
+
+# Plot the figure and saving in a html file
+fig = go.Figure(data=data, layout=layout)
+pyo.plot(fig, filename='bloomington_dayofweek_barchart.html')
--- clover-traffic-connections-code/heatmap.py
+++ clover-traffic-connections-code/heatmap.py
@@ -4,13 +4,16 @@ import pandas as pd
 
 # Load CSV file from Datasets folder
 df = pd.read_csv('../datasets/monroe-county-crash-data2003-to-2015.csv', encoding="ISO-8859–1")
+
 # Preparing data
 data = [go.Heatmap(x=df['Collision Type'],
                    y=df['Injury Type'],                   
                    z=df['Hour'].values.tolist(),
                    colorscale='Jet')]
+
 # Preparing layout
-layout = go.Layout(title='Time of crashes in Bloomington by vehicles and injury', xaxis_title="Vehicles Involved", yaxis_title="Type of Injury")
+layout = go.Layout(title='Time of Crashes in Bloomington by Vehicle and Injury', xaxis_title="Vehicles Involved", yaxis_title="Type of Injury")
+
 # Plot the figure and saving in a html file
 fig = go.Figure(data=data, layout=layout)
 pyo.plot(fig, filename='bloomington_heatmap.html')
