114576e4b8551cfd308b8344f60069ded5d86ab2
Rachel Tomas <rtomas2@uncc.edu>
2020-12-20 18:04:17 -0500

    adding home content

--- clover-traffic-connections-code/traffic-connections.js
+++ clover-traffic-connections-code/traffic-connections.js
@@ -4,11 +4,9 @@ nav_html += '<a href="bloomington_crashesbyreasonandstreet_barchart.html">Crash 
 nav_html += '<a href="bloomington_timeandtype.html">Time and Type of Crashes</a>';
 nav_html += '<a href="bloomington_crashesbydaymonthandyear_barchart.html">Crash Statistics Over Time</a>';
 
-var footer_html = '';
-footer_html += '<p>This site was made as a school project for the city of Bloomington, Indiana</p>';
+
 
 window.onload = function(){
     document.getElementById("nav-bar").innerHTML = nav_html;
-    document.getElementById("footer-bar").innerHTML = footer_html;
 }
 
