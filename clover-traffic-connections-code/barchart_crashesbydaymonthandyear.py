e6037c48d84287420d4f7b97e981518344ef56ce
Seneschal55 <ultimateassassin22@gmail.com>
2020-12-19 21:51:07 -0500

    Modified files to make it easier to portray bars in the accurate manner

--- clover-traffic-connections-code/barchart_crashesbydaymonthandyear.py
+++ clover-traffic-connections-code/barchart_crashesbydaymonthandyear.py
@@ -12,7 +12,6 @@ filtered_df = df[df['Injury Type'] != 'No injury/unknown']
 
 # Creating sum of number of crashes group
 new_df = filtered_df.groupby(['Month'])['Injury Type'].count().reset_index()
-filtered_df = filtered_df.sort_values(by=['Injury Type'], ascending=[True]).head().reset_index()
 
 df2 = pd.read_csv('../Datasets/monroe-county-crash-data2003-to-2015.csv', encoding="ISO-8859–1")
 
@@ -29,8 +28,8 @@ df3 = pd.read_csv('../Datasets/monroe-county-crash-data2003-to-2015.csv', encodi
 filtered_df3 = df3[df3['Injury Type'] != 'No injury/unknown']
 
 # Creating sum of number of crashes group
-new_df3 = filtered_df.groupby(['Day'])['Injury Type'].count().reset_index()
-new_df3 = new_df3.sort_values(by=['Injury Type'], ascending=[True]).head().reset_index()
+new_df3 = filtered_df3.groupby(['Day'])['Injury Type'].count().reset_index()
+new_df3 = new_df3.sort_values(by=['Injury Type'], ascending=[True]).head(7).reset_index()
 
 # Plot the figure and saving in a html file
 fig = make_subplots(rows=1, cols=3, shared_yaxes=False, subplot_titles=("Number of crashes with Injury in Bloomington by Day", "Number of Crashes with Injury in Bloomington by Month", "Number of Crashes with Injury in Bloomington by Year"))
@@ -38,9 +37,10 @@ fig.add_trace(go.Bar(x=new_df3['Day'], y=new_df3['Injury Type']), 1, 1)
 fig.add_trace(go.Bar(x=new_df['Month'], y=new_df['Injury Type']), 1, 2)
 fig.add_trace(go.Bar(x=new_df2['Year'], y=new_df2['Injury Type']), 1, 3)
 fig.update_xaxes(title_text="Day", row=1, col=1)
-fig.update_xaxes(title_text="Month Number", row=1, col=2)
+fig.update_xaxes(title_text="Month", row=1, col=2)
 fig.update_xaxes(title_text="Year", row=1, col=3)
 fig.update_yaxes(title_text="Number of Crashes with Injuries", row=1, col=1)
+fig.update_yaxes(title_text="Number of Crashes with Injuries", row=1, col=2)
 fig.update_yaxes(title_text="Number of Crashes with Injuries", row=1, col=3)
-fig.update_yaxes(title_text="Number of Crashes with Injuries", row=1, col=3)
+fig.update_layout(showlegend=False)
 pyo.plot(fig, filename='bloomington_crashesbydaymonthandyear_barchart.html')
